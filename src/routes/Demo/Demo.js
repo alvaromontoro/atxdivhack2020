09a92d0ec3e5087990d39196cdc63cd6c44706d4
Alvaro Montoro <alvaromontoro@gmail.com>
2020-09-20 12:44:40 -0500

    Demo working! :D

--- src/routes/Demo/Demo.js
+++ src/routes/Demo/Demo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BackButton from "../../components/Back/Back";
 import { updateMonetization } from "../../utils/monetization";
 
@@ -9,7 +9,8 @@ export const Demo = ({
     params: { id },
   },
 }) => {
-  // const mat = window.ddrmat;
+  const [direction, setDirection] = useState(Math.floor(Math.random() * 4));
+
   let data = {};
   // load the data
   if (id) {
@@ -19,7 +20,22 @@ export const Demo = ({
   }
   updateMonetization(data.monetization);
 
-  //mat.on("")
+  const newDirection = () => {
+    const dir = Math.floor(Math.random() * 4);
+    setDirection(dir);
+  };
+
+  const buttonPressed = (num) => {
+    if (num === direction) {
+      newDirection();
+    }
+  };
+
+  window.ddrmat
+    .before("button2", () => buttonPressed(0))
+    .before("button3", () => buttonPressed(1))
+    .before("button1", () => buttonPressed(2))
+    .before("button0", () => buttonPressed(3));
 
   return (
     <div className={`demo ${data.multimedia.video && "video"}`}>
@@ -52,7 +68,12 @@ export const Demo = ({
             </svg>
           </div>
           <div id="top-center">
-            <svg width="100%" height="100%" viewBox="0 0 100 100">
+            <svg
+              width="100%"
+              height="100%"
+              viewBox="0 0 100 100"
+              className={direction === 0 ? "active" : undefined}
+            >
               <path
                 d="M 50,20 80,50 65,50 65,70 35,70 35,50 20,50 Z"
                 stroke="#fff"
@@ -67,7 +88,12 @@ export const Demo = ({
             </svg>
           </div>
           <div id="center-left">
-            <svg width="100%" height="100%" viewBox="0 0 100 100">
+            <svg
+              width="100%"
+              height="100%"
+              viewBox="0 0 100 100"
+              className={direction === 3 ? "active" : undefined}
+            >
               <path
                 d="M 20,50 50,20 50,35 70,35 70,65 50,65 50,80 20,50 Z"
                 stroke="#fff"
@@ -80,7 +106,12 @@ export const Demo = ({
             Web DDR <abbr title="Austin Texas Diversity Hackathon 2020">ATXDivHack 2020</abbr>
           </div>
           <div id="center-right">
-            <svg width="100%" height="100%" viewBox="0 0 100 100">
+            <svg
+              width="100%"
+              height="100%"
+              viewBox="0 0 100 100"
+              className={direction === 1 ? "active" : undefined}
+            >
               <path
                 d="M 80,50 50,20 50,35 30,35 30,65 50,65 50,80 80,50 Z"
                 stroke="#fff"
@@ -95,7 +126,12 @@ export const Demo = ({
             </svg>
           </div>
           <div id="bottom-center">
-            <svg width="100%" height="100%" viewBox="0 0 100 100">
+            <svg
+              width="100%"
+              height="100%"
+              viewBox="0 0 100 100"
+              className={direction === 2 ? "active" : undefined}
+            >
               <path
                 d="M 50,80 80,50 65,50 65,30 35,30 35,50 20,50 Z"
                 stroke="#fff"
