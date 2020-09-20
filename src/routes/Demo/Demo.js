b333a1294f62f31598ec4f776ff635e22ee94d39
Alvaro Montoro <alvaromontoro@gmail.com>
2020-09-20 11:46:45 -0500

    Add DDR pad to demo

--- src/routes/Demo/Demo.js
+++ src/routes/Demo/Demo.js
@@ -19,13 +19,81 @@ export const Demo = ({
   updateMonetization(data.monetization);
 
   return (
-    <div className="demo">
+    <div className={`demo ${data.multimedia.video && "video"}`}>
       <h1>
         {data.title}
         <span>by {data.artist}</span>
       </h1>
       <div id="pad">
-        <svg viewBox="0 0 100 100"></svg>
+        <div id="top-bar">
+          <div>Select</div>
+          <div>Start</div>
+        </div>
+        <div id="buttons">
+          <div id="top-left">
+            <svg width="100%" height="100%" viewBox="0 0 100 100">
+              <path d="M 30,30 70,70 M30,70 70,30" stroke="#222" stroke-width="7" />
+            </svg>
+          </div>
+          <div id="top-center">
+            <svg width="100%" height="100%" viewBox="0 0 100 100">
+              <path
+                d="M 50,20 80,50 65,50 65,70 35,70 35,50 20,50 Z"
+                stroke="#fff"
+                stroke-width="5"
+                fill="none"
+              />
+            </svg>
+          </div>
+          <div id="top-right">
+            <svg width="100%" height="100%" viewBox="0 0 100 100">
+              <circle cx="50" cy="50" r="20" stroke="#222" stroke-width="7" fill="none" />
+            </svg>
+          </div>
+          <div id="center-left">
+            <svg width="100%" height="100%" viewBox="0 0 100 100">
+              <path
+                d="M 20,50 50,20 50,35 70,35 70,65 50,65 50,80 20,50 Z"
+                stroke="#fff"
+                stroke-width="5"
+                fill="none"
+              />
+            </svg>
+          </div>
+          <div id="center-center">
+            Web DDR <abbr title="Austin Texas Diversity Hackathon 2020">ATXDivHack 2020</abbr>
+          </div>
+          <div id="center-right">
+            <svg width="100%" height="100%" viewBox="0 0 100 100">
+              <path
+                d="M 80,50 50,20 50,35 30,35 30,65 50,65 50,80 80,50 Z"
+                stroke="#fff"
+                stroke-width="5"
+                fill="none"
+              />
+            </svg>
+          </div>
+          <div id="bottom-left">
+            <svg width="100%" height="100%" viewBox="0 0 100 100">
+              <path d="M 50,30 70,70 30,70 Z" stroke="#222" stroke-width="7" fill="none" />
+            </svg>
+          </div>
+          <div id="bottom-center">
+            <svg width="100%" height="100%" viewBox="0 0 100 100">
+              <path
+                d="M 50,80 80,50 65,50 65,30 35,30 35,50 20,50 Z"
+                stroke="#fff"
+                stroke-width="5"
+                fill="none"
+              />
+            </svg>
+          </div>
+          <div id="bottom-right">
+            <svg width="100%" height="100%" viewBox="0 0 100 100">
+              <path d="M 30,30 30,70 70,70 70,30 Z" stroke="#222" stroke-width="7" fill="none" />
+            </svg>
+          </div>
+        </div>
       </div>
       <BackButton />
     </div>
