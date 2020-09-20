7875e9459c346535d0d6700c9351bb75e9cafb86
Alvaro Montoro <alvaromontoro@gmail.com>
2020-09-20 11:52:08 -0500

    Fix styles with and without video

--- src/routes/Demo/Demo.js
+++ src/routes/Demo/Demo.js
@@ -9,6 +9,7 @@ export const Demo = ({
     params: { id },
   },
 }) => {
+  // const mat = window.ddrmat;
   let data = {};
   // load the data
   if (id) {
@@ -17,6 +18,8 @@ export const Demo = ({
     data = window.localData;
   }
   updateMonetization(data.monetization);
+
+  //mat.on("")
 
   return (
     <div className={`demo ${data.multimedia.video && "video"}`}>
@@ -32,7 +35,7 @@ export const Demo = ({
         <div id="buttons">
           <div id="top-left">
             <svg width="100%" height="100%" viewBox="0 0 100 100">
-              <path d="M 30,30 70,70 M30,70 70,30" stroke="#222" stroke-width="7" />
+              <path d="M 30,30 70,70 M30,70 70,30" stroke="#222" strokeWidth="7" />
             </svg>
           </div>
           <div id="top-center">
@@ -40,14 +43,14 @@ export const Demo = ({
               <path
                 d="M 50,20 80,50 65,50 65,70 35,70 35,50 20,50 Z"
                 stroke="#fff"
-                stroke-width="5"
+                strokeWidth="5"
                 fill="none"
               />
             </svg>
           </div>
           <div id="top-right">
             <svg width="100%" height="100%" viewBox="0 0 100 100">
-              <circle cx="50" cy="50" r="20" stroke="#222" stroke-width="7" fill="none" />
+              <circle cx="50" cy="50" r="20" stroke="#222" strokeWidth="7" fill="none" />
             </svg>
           </div>
           <div id="center-left">
@@ -55,7 +58,7 @@ export const Demo = ({
               <path
                 d="M 20,50 50,20 50,35 70,35 70,65 50,65 50,80 20,50 Z"
                 stroke="#fff"
-                stroke-width="5"
+                strokeWidth="5"
                 fill="none"
               />
             </svg>
@@ -68,14 +71,14 @@ export const Demo = ({
               <path
                 d="M 80,50 50,20 50,35 30,35 30,65 50,65 50,80 80,50 Z"
                 stroke="#fff"
-                stroke-width="5"
+                strokeWidth="5"
                 fill="none"
               />
             </svg>
           </div>
           <div id="bottom-left">
             <svg width="100%" height="100%" viewBox="0 0 100 100">
-              <path d="M 50,30 70,70 30,70 Z" stroke="#222" stroke-width="7" fill="none" />
+              <path d="M 50,30 70,70 30,70 Z" stroke="#222" strokeWidth="7" fill="none" />
             </svg>
           </div>
           <div id="bottom-center">
@@ -83,14 +86,14 @@ export const Demo = ({
               <path
                 d="M 50,80 80,50 65,50 65,30 35,30 35,50 20,50 Z"
                 stroke="#fff"
-                stroke-width="5"
+                strokeWidth="5"
                 fill="none"
               />
             </svg>
           </div>
           <div id="bottom-right">
             <svg width="100%" height="100%" viewBox="0 0 100 100">
-              <path d="M 30,30 30,70 70,70 70,30 Z" stroke="#222" stroke-width="7" fill="none" />
+              <path d="M 30,30 30,70 70,70 70,30 Z" stroke="#222" strokeWidth="7" fill="none" />
             </svg>
           </div>
         </div>
