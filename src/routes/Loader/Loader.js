b76a66d28fb87a34fcb4c2143165e55c359a6aca
Alvaro Montoro <alvaromontoro@gmail.com>
2020-09-20 13:31:13 -0500

    Setting the loader

--- src/routes/Loader/Loader.js
+++ src/routes/Loader/Loader.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import BackButton from "../../components/Back/Back";
 import "./Loader.css";
 
-export const Loader = () => (
-  <div className="loader">
-    <h1>Loader</h1>
-    <p>Here you can load your JSON.</p>
-    <BackButton />
-  </div>
-);
+export const Loader = () => {
+  const [data, setData] = useState("");
+
+  return (
+    <div className="loader">
+      <h1>Loader</h1>
+      <div className="form">
+        <p>Here you can load your JSON.</p>
+        <textarea
+          placeholder="Enter your JSON here"
+          aria-label="JSON with Web DDR information"
+        ></textarea>
+        <button>Process JSON</button>
+      </div>
+      <BackButton />
+    </div>
+  );
+};
 
 export default Loader;
